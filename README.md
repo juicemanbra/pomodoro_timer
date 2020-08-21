44991a963b8737694681654a37a3d611456c5a27
juicemanbra <68497217+juicemanbra@users.noreply.github.com>
2020-08-21 10:58:38 +0200

    notifications for macOS

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # pomodoro_timer
+a simple timer to use for the pomdoro technique for better memory & productivity
+
+it also automatically saves the time spent working and the name of the task to a csv file for later analysis
--- main.py
+++ main.py
@@ -3,12 +3,31 @@ import datetime as dt
 import os
 import csv
 
-def countdown(interval, type):
+
+def countdown(interval, type): # this is a simple countdown that displays minutes and seconds
     while interval:
         mins, secs = divmod(interval, 60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
         t.sleep(1)
         interval -= 1
-        print(type, timer, end='\r')
+        print(type, timer, end='\r') # takes the type of interval (pasue/work) and the time
 
+
+def pause():
+    pass
+
+def work():
+    pass
+
+def savetoCSV():
+    pass
+
+def start():
+    pass
+
+def notifier(title, text):
+        os.system("""
+                osascript -e 'display notification "{}" with title "{}" sound name "Glass"'
+                """.format(text, title))
+    
 
